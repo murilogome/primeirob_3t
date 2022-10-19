a2e07d75a865bc9b1d46871f69eb4bf0b1355b3a
murilogome <110116007+murilogome@users.noreply.github.com>
2022-10-19 12:39:12 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,35 +1,41 @@
-let tabuada = 5;
-
-function escreva (){
-    document.write ("Tabuada do " + tabuada + "<br>")
-    document.write(tabuada + " x 1 =  " + (tabuada*1)+ "<br>");
+let tabuada = 111;
+function escreva(){
+    document.write("Tabuada do " + tabuada + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 =  " + (tabuada*3)+ "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
     document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 =  " + (tabuada*5)+ "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
     document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 =  " + (tabuada*7)+ "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
     document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 =  " + (tabuada*9)+ "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-
 }
 
-let lista = ["jesus", "neuza", "gilmar" , "duarte" , "jorge" , "spessato"];
-function mostraLista (){
-    document.write ("Tamanho da lista: " + lista.length + "<br>");
-    
+let lista = ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"];
+
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
     for(let i = 0; i < lista.length; i++){
-        document.write ("professor" + lista[i] + "<br>");
+        document.write("Professor: " + lista[i] + "<br>");
     }
 }
 
-function repete (){
-    for(let i = 1; i < 10; i ++){
+function multiplica(){
+    for(let i = 6; i <= 8; i++){
         document.write("Tabuada do " + i + "<br>");
-        for(let j = 1; j <= 10; j ++){
-            document.write(i + " x " + j + " = " + (j*j) + "<br>");
-
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x "+j+" = " + (i*j) + "<br>");
         }
+        document.write("<br>");
+    function media(){
+        let n1 = document getElementById("n1").value
+        let n2 = document getElementById("n2").value
+        let n3 = document getElementById("n3").value
+        let n4 = document getElementById("n4").value
+        let r = (Number(n1))+Number(n2)+Number(n3)+Number(n4))/4;
+        document.getElementById("resul").innerHTML = "media": " r;
     }
 }
+
